ee07cb47c7955c55f66175670c4d7bbcd10e1f30
Illya Shuba <105970858+IlliaShuba@users.noreply.github.com>
2022-11-22 12:32:42 +0200

    object pool python

--- python/objectPool/main.py
+++ python/objectPool/main.py
@@ -1,43 +1,43 @@
 class Order:
-    def __init__(self, order_type, description, price):
-        self.order_type = order_type
+    def __init__(self, description, price):
         self.description = description
         self.price = price
-        self._status = "Running"
+        self._status = "Ready"
 
+    def startProcessing(self):
+        self._status = "Running"
+    def finish(self):
+        self._status = "Ready"
     def is_ready(self):
         return self._status == "Ready"
-
-    def update_title(self, title):
-        self._title = title
-
 
 class OrderPool:
     def __init__(self):
         pass
 
-    _pool = [Order(1, "ZOOM", ""), Order(2, "HANGOUTS", "")]
+    _pool = [Order("some description 1", 100), Order("some description 2", 200)]
 
     def _is_free(self, meeting):
         return meeting.is_ready()
 
-    def start_meeting_for(self, title) -> Order:
-        freeMeetings = list(filter(self._is_free, OrderPool._pool))
-        if freeMeetings:
-            meeting = freeMeetings[0]
-            meeting.update_title(title)
-            meeting.start_meeting()
-            return meeting
+    def get_order(self) -> Order:
+        free_order = list(filter(self._is_free, OrderPool._pool))
+        if free_order:
+            order = free_order[0]
+            order.startProcessing()
+            print(order.description)
+            return order
         else:
-            print("No free meetings")
+            print("No free order")
 
-    def release_meeting(self, meeting):
-        meeting.finish_meeting()
+    def release_order(self, order):
+        order.finish()
+        print("finish order")
 
 
 if __name__ == '__main__':
     pool = OrderPool()
-    algebra = pool.start_meeting_for("Algebra")
-    algebra2 = pool.start_meeting_for("Algebra2")
-    pool.release_meeting(algebra2)
-    algebra3 = pool.start_meeting_for("Algebra3")
+    order1 = pool.get_order()
+    order2 = pool.get_order()
+    order3 = pool.get_order()
+    pool.release_order(order1)
