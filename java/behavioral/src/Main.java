267cc4938bdeed2d200eeb1e24f6ac1b34d8c33a
Illya Shuba <105970858+IlliaShuba@users.noreply.github.com>
2022-12-04 01:01:37 +0200

    memento

--- java/behavioral/src/Main.java
+++ java/behavioral/src/Main.java
@@ -5,11 +5,13 @@ import mediator.Admin;
 import mediator.SimpleTextChat;
 import mediator.SimpleUser;
 import mediator.User;
+import memento.GithudRepo;
+import memento.Project;
 import сommand.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Main.Mediator();
+    public static void main(String[] args) throws InterruptedException {
+        Main.Memento();
     }
 
     public static void Chain() {
@@ -66,6 +68,35 @@ public class Main {
 
         user1.sendMessage("Hello, I am user 1!!!");
         admin.sendMessage("Roger that. I am admin!");
+    }
+
+    public static void Memento() throws InterruptedException {
+        Project project = new Project();
+        GithudRepo github = new GithudRepo();
+
+        System.out.println("Creating new project. Version  1.0");
+        project.setVersionAndDate("Version 1.0");
+
+        System.out.println("Saving current version to GitHub...");
+        github.setSave(project.save());
+
+        System.out.println("updating project to version 1.1");
+
+        System.out.println("Write poor code...");
+        Thread.sleep(5000);
+
+
+        project.setVersionAndDate("Version 1.1");
+
+        System.out.println(project);
+
+        System.out.println("Something went wrong...");
+
+        System.out.println("Rolling back to version 1.0");
+        project.load(github.getSave());
+
+        System.out.println("Project after rollback.");
+        System.out.println(project);
     }
 
 }
---
+++ python/behavioral/memento.py
@@ -0,0 +1,50 @@
+class GithudRepo:
+    save = None
+
+    def getSave(self):
+        return self.save
+
+    def setSave(self, save):
+        self.save = save
+
+
+class Save:
+    version = None
+
+    def __init__(self, version):
+        self.version = version
+
+    def getVersion(self):
+        return self.version
+
+
+class Project:
+    version = None
+
+    def setVersionAndDate(self, version):
+        self.version = version
+
+    def save(self):
+        return Save(self.version)
+
+    def load(self, save):
+        self.version = save.getVersion()
+
+
+if __name__ == "__main__":
+    project = Project()
+    github = GithudRepo()
+    print("Creating new project. Version  1.0")
+    project.setVersionAndDate("Version 1.0")
+    print("Saving current version to GitHub...")
+    github.setSave(project.save())
+    print("updating project to version 1.1")
+    print("Write poor code...")
+    # Thread.sleep(5000)
+    project.setVersionAndDate("Version 1.1")
+    print(project)
+    print("Something went wrong...")
+    print("Rolling back to version 1.0")
+    project.load(github.getSave())
+    print("Project after rollback.")
+    print(project)
